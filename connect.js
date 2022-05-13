3412c9f9f09ea2f11e25933fbfb466cdeec77a9d
nv08 <nyvy.07@gmail.com>
2022-05-14 01:46:30 +0530

    fix: env

--- connect.js
+++ connect.js
@@ -1,12 +1,13 @@
 var { Client } = require("pg");
 
 const client = new Client({
-  connectionString: process.env.DATABASE_URI,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
+    connectionString:
+      "postgres://qipnurrwvizohh:fb294667d3a334f3edd4ae315de4cc8cba35b56eba5a3e0b07a804a75f1c587a@ec2-44-196-223-128.compute-1.amazonaws.com:5432/d2gu35vrnttkk5",
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
 
-client.connect();
+  client.connect()
 
-module.exports = client;
+  module.exports = client
